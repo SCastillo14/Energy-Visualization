177aab6d49bd04fbf44bc68d71ca3d88fbc96c9a
SCastillo14 <83621858+SCastillo14@users.noreply.github.com>
2022-12-04 21:27:27 -0600

    cambio en cooling degrees por Generación eólica total

--- js/explore1vis.js
+++ js/explore1vis.js
@@ -155,7 +155,7 @@ Explore1Vis.prototype.updateVis = function(){
 
     // updates axis
     var unit = {"temperature": "(" + String.fromCharCode(176) + "MWh)", "dehumidification": "(MWh)",
-        "cooling degrees": "(" + String.fromCharCode(176) + "MWh)", "heating degrees": "(" + String.fromCharCode(176) + "MWh)",
+        "Generación eólica total": "(" + String.fromCharCode(176) + "MWh)", "heating degrees": "(" + String.fromCharCode(176) + "MWh)",
         "solar radiation": "(MWh)", "wind speed": "(MWh)",
         "generacion" : "(MWh)", "Capacidad": "(MWh)", "steam" : "(MWh)"
     };
