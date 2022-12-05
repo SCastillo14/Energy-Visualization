46b2e6d48f0a3230f17a89017e004ff62b8766e0
SCastillo14 <83621858+SCastillo14@users.noreply.github.com>
2022-12-04 23:28:57 -0600

    cambio weathervis

--- js/weathervis.js
+++ js/weathervis.js
@@ -1,4 +1,4 @@
-TermoeléctricaTermoeléctricaWeatherVis = function(_parentElement, _data, _option, _eventHandler){
+WeatherVis = function(_parentElement, _data, _option, _eventHandler){
     this.parentElement = _parentElement;
     this.data = _data;
 	this.option = _option;
